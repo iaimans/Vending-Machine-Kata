fa98a39d656d9d349985d9f48245bc18c8c3fd25
iaimans <isaac@leanmind.es>
2024-01-15 10:57:46 +0000

    Use Nunit instead of Xunit

--- Kata.Unit.Tests/VendingMachineShould.cs
+++ Kata.Unit.Tests/VendingMachineShould.cs
@@ -1,18 +1,20 @@
+using FluentAssertions;
+using NUnit.Framework;
+
 namespace Kata.Unit.Tests;
 
-public class CalculatorShould
+public class VendingMachineShould
 {
-    
+
     public static IEnumerable<object[]> insertedAndRejectedCoins()
     {
-        yield return new object[] { new List<Coin> { Coin.Nickel, Coin.Dime }, new List<Coin> {} };
+        yield return new object[] { new List<Coin> { Coin.Nickel, Coin.Dime }, new List<Coin> { } };
         yield return new object[] { new List<Coin> { Coin.Penny }, new List<Coin> { Coin.Penny } };
-        yield return new object[] { new List<Coin> { Coin.Penny, Coin.Nickel, Coin.Dime, Coin.Penny}, new List<Coin> { Coin.Penny, Coin.Penny } };
+        yield return new object[] { new List<Coin> { Coin.Penny, Coin.Nickel, Coin.Dime, Coin.Penny }, new List<Coin> { Coin.Penny, Coin.Penny } };
 
     }
-
-    [Theory]
-    [MemberData(nameof(insertedAndRejectedCoins))]
+    
+    [TestCaseSource(nameof(insertedAndRejectedCoins))]
     public void rejected_coins_are_placed_into_coin_return(List<Coin> insertedCoins, List<Coin> rejectedCoins)
     {
         var vendingMachine = new VendingMachine();
